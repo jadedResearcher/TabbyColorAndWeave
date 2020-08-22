6b76dd3e3a82fd326af9f2a844516bc390b175af
jadedResearcher <gtg287y@gmail.com>
2020-08-22 10:57:06 -0400

    can click and drag heddles around

--- web/scripts/RigidHeddle/View/RigidHeddleLoomView.dart
+++ web/scripts/RigidHeddle/View/RigidHeddleLoomView.dart
@@ -11,13 +11,17 @@ import 'WarpChainView.dart';
      Element parent;
      RigidHeddleLoom loom;
      RigidHeddleLoomView(this.loom, this.parent);
+     int heddlesX;
+     int heddlesY;
+     SvgElement heddleContainer;
+     bool draggingHeddles = false;
 
      void renderLoom() {
         final SvgElement loomElement = SvgElement.tag("svg")..classes.add("loom");
         loomElement.attributes["width"] = "1000";
         loomElement.attributes["height"] = "500";
         parent.append(loomElement);
-        final SvgElement heddleContainer = SvgElement.tag("g")..classes.add("heddles");
+        heddleContainer = SvgElement.tag("g")..classes.add("heddles");
         loomElement.append(heddleContainer);
         int y = 200;
         for(Heddle heddle in loom.heddles) {
@@ -32,6 +36,30 @@ import 'WarpChainView.dart';
             x = new WarpChainView(chain, warpContainer,x, 450).renderChain();
         }
 
+        setupControls();
+        handleMove(150, 0);
+
 
     }
+
+    void setupControls() {
+
+         heddleContainer.onMouseDown.listen((Event e) {
+             draggingHeddles = true;
+         });
+
+         window.onMouseUp.listen((Event e) {
+             draggingHeddles = false;
+         });
+
+         window.onMouseMove.listen((MouseEvent e) {
+             if(draggingHeddles) handleMove((e.offset.x).ceil(), (e.offset.y).ceil());
+         });
+    }
+
+     void handleMove(int newX, int newY) {
+         heddlesX = newX;
+         heddlesY = newY;
+         heddleContainer.attributes["transform"] = "translate($newX,$newY)";
+     }
 }
