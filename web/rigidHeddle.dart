cc6a28342996bc22012e5c7fda0614ad653578d7
jadedResearcher <gtg287y@gmail.com>
2020-09-02 22:53:25 -0400

    it worksgit add -A!

--- web/rigidHeddle.dart
+++ web/rigidHeddle.dart
@@ -1,10 +1,12 @@
 import 'dart:html';
 
+import 'package:ImageLib/Encoding.dart';
 import 'package:LoaderLib/Loader.dart';
 
 import 'scripts/Fabric.dart';
 import 'scripts/RigidHeddle/Model/RigidHeddleLoom.dart';
 import 'scripts/RigidHeddle/View/RigidHeddleLoomView.dart';
+String templateSource = "http://farragofiction.com/RigidHeddleSim/Templates/";
 
 List<Pattern> patterns = [];
 Fabric fabric;
@@ -13,7 +15,11 @@ RigidHeddleLoomView view;
 void main() {
     view = new RigidHeddleLoomView(RigidHeddleLoom.testSingleLoom());
     view.renderLoom(output);
-    processTemplates();
+    try {
+        processTemplates();
+    }catch(error) {
+        window.console.error("Error caught trying to process templates $error");
+    }
 }
 
 void processTemplates() async {
@@ -25,15 +31,13 @@ void processTemplates() async {
 }
 
 void getTemplates(Element container) async{
-    String imageListSource = "http://farragofiction.com/RigidHeddleSim/Templates/list.php";
-    Map<String,dynamic> results = await Loader.getResource(imageListSource,format: Formats.json );
+    final Map<String,dynamic> results = await Loader.getResource("$templateSource/list.php",format: Formats.json );
     for(String folder in results["folders"].keys) {
         processFolder(container, folder, results);
     }
 }
 
 void processFolder(Element container, String folder,Map<String,dynamic>  results) {
-    print("in folder $folder");
     DivElement folderContainer = new DivElement()..classes.add("folder");
     HeadingElement header = new HeadingElement.h3()..text = folder;
     folderContainer.append(header);
@@ -41,13 +45,22 @@ void processFolder(Element container, String folder,Map<String,dynamic>  results
     container.append(folderContainer);
     for(String file in results["folders"][folder]["files"]) {
         if (file.contains("png")) {
-            processfile(folderContainer, file);
+            processfile(folderContainer, folder, file);
         }
     }
 }
 
-void processfile(Element container, String file) {
+void processfile(Element container, String folder, String file) {
     ButtonElement button = new ButtonElement()..classes.add("file")..text = "${file.replaceAll(".png","")}";
     container.append(button);
+    button.onClick.listen((Event e) {
+        loadTemplate("$templateSource$folder/$file");
+    });
 
+
+}
+
+void loadTemplate(String location) async {
+    ArchivePng image = await Loader.getResource(location, format: ArchivePng.format);
+    view.syncLoomToImage(image, location);
 }
